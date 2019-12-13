bdfd80cf21fe8275455b409a6ce44e66ed86a569
Cory Goates <cory.goates@aggiemail.usu.edu>
2019-12-13 16:00:10 -0700

    Added necessary files.

---
+++ pysim/simulator.py
@@ -0,0 +1,13 @@
+# Class containing the simulator
+
+import math as m
+import numpy as mp
+
+class Simulator:
+    """Class defining a simulator.
+    """
+
+    def __init__(self, input_dict):
+
+        # Store input
+        self._input_dict = input_dict
