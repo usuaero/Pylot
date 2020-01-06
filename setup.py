09cf17fa4dcc94bc04985bd579ed7455399de853
Cory Goates <cory.goates@aggiemail.usu.edu>
2020-01-06 13:30:55 -0700

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Pylot: A light and fast aircraft flight simulator."""
+
+from setuptools import setup
+import os
+import sys
+
+setup(name = 'Pylot',
+    version = '1.0.1',
+    description = "Pylot: A light and fast aircraft flight simulator.",
+    url = 'https://github.com/usuaero/Pylot',
+    author = 'usuaero',
+    author_email = 'doug.hunsaker@usu.edu',
+    install_requires = ['numpy', 'scipy', 'pytest', 'matplotlib', 'pandas', 'numpy-stl'],
+    python_requires ='>=3.6.0',
+    license = 'MIT',
+    packages = ['pylot'],
+    zip_safe = False)
