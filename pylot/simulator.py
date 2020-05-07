c9c0b37d0278de97f288928f4697e8dcb2927913
Cory Goates <cory.goates@aggiemail.usu.edu>
2020-05-07 16:24:53 -0600

    Solved the case of the vanishing airplane.

--- pylot/simulator.py
+++ pylot/simulator.py
@@ -119,7 +119,7 @@ class Simulator:
             self._sky = self._create_mesh("sky.obj", "sky.vs", "sky.fs", "clouds_low_res.jpg", [0.0, 0.0, 0.0], [1.0, 0.0, 0.0, 0.0])
             self._render_loading_message('airstrip')
             self._airstrip = self._create_mesh("airstrip.obj", "field.vs", "field.fs", "landing.jpg", [0.0, 0.0, -1.0], [1.0, 0.0, 0.0, 0.0])
-            self._render_loading_message('props')
+            self._render_loading_message('structures')
             self._tent = self._create_mesh("tent.obj", "aircraft.vs", "aircraft.fs", "tent.jpg", [0.0, 25.0, 0.0], [1.0, 0.0, 0.0, 0.0])
             self._bench = self._create_mesh("wood_bench.obj", "aircraft.vs", "aircraft.fs", "bench.jpg", [10.0, 25.0, 0.0], [1.0, 0.0, 0.0, 0.0])
             self._render_loading_message('trees')
