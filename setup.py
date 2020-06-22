49174cda9ebfca7f09c08aa713b3c67c7b4d9f46
Cory Goates <cory.goates@aggiemail.usu.edu>
2020-06-22 15:13:13 -0600

    Added bungee launch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 setup(name = 'Pylot',
-    version = '1.3.0',
+    version = '1.3.1',
     description = "Pylot: A light and fast aircraft flight simulator.",
     url = 'https://github.com/usuaero/Pylot',
     author = 'usuaero',
