2e623fa2f3c378aee49444ccef0ecd2e9a7052b9
Cory Goates <cory.goates@aggiemail.usu.edu>
2021-03-22 15:54:23 -0600

    Updated UI for accessing MachUpX solver options.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Cory Goates, Doug Hunsaker'
 author = 'Cory Goates, Doug Hunsaker'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.4'
+release = '1.4.0'
 
 
 # -- General configuration ---------------------------------------------------
